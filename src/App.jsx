ac4e8b71910a4eb371de9f3ae686db161fbf0e3e
Manas099 <manas.sri07@gmail.com>
2026-06-27 23:38:24 +0530

    first card updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ const App = () => {
   return (
     <div className='parent'>
       <div className="card">
-        <div className="top">
+       <div>
+         <div className="top">
           <img src="https://up.yimg.com/ib/th/id/OIP.mfjIDgZfzL5i-BViz7SCnwHaD4?pid=Api&rs=1&c=1&qlt=95&w=205&h=107" alt="" />
           <button>save<Bookmark size={10}/></button>
         </div>
        <div className="centre">
         <h3>Amaazon <span>5 days ago</span></h3>
         <h2>Senior UI/UX designer</h2>
+        <div className='tag'>
         <h4>part-time</h4>
         <h4>senior level</h4>
+        </div>
+       </div>
        </div>
       <div className="bottom">
           <div>
